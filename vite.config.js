c96150d938141fbec1c4427a1149de3a7b7630b7
George(chiyum) <pray870712@gmail.com>
2025-08-28 15:04:27 +0800

    ci: 🎡 打包輸出資料夾

--- vite.config.js
+++ vite.config.js
@@ -49,7 +49,7 @@ export default defineConfig(({ mode }) => {
           drop_debugger: true,
         },
       },
-      outDir: "./docs", // 您想要的输出目录
+      outDir: "./dist", // 您想要的输出目录
       chunkSizeWarningLimit: 10000,
     },
   };
